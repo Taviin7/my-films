6f2a8da70a3152d39e9f2ff85db2e555f7c28f8a
aluno <aluno@LAB.ETEC187>
2022-09-05 22:24:17 -0300

    Finalizando a interface da tela Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,13 +10,17 @@ import {
 export default function Home(){
     return(
 
-        <View>
-            <Text>My Films Favorites</Text>
-            <Text>Olá Tavin, Boa Noite</Text>
+        <View style = {styles.container}>
+            <Text style = {styles.titulo}>My Films Favorites</Text>
+            <Text style = {styles.subtitulo}>Olá Tavin, Boa Noite</Text>
 
+        <TextInput
+        style={styles.campo}
+        placeholder = "Nome do Filme"
+        />
 
-        <TouchableOpacity>
-            <Text>Adicionar</Text>
+        <TouchableOpacity style = {styles.button}>
+            <Text style = {styles.buttonText}>Adicionar</Text>
         </TouchableOpacity>
 
         </View>
@@ -25,6 +29,52 @@ export default function Home(){
 
 //Criando os etilos do layout
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#483D8B',
+        paddingVertical: 70,
+        paddingHorizontal: 20
+    },
 
+    titulo:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        color: '#FFDAB9'
+        
+    },
 
-});
+    subtitulo:{
+        color:'#FFF',
+        fontSize: 15
+    },
+
+    campo:{
+        backgroundColor: '#1F1E25',
+        color: '#FFF',
+        fontSize: 18,
+        marginTop: 30,
+        borderRadius: 7,
+        padding: 15,
+        marginBottom: 5
+    },
+
+    greeting:{
+        color: '#FFF'
+    },
+
+    button:{
+        backgroundColor: '#A370F7',
+        padding: 15,
+        borderRadius: 7,
+        alignItems: 'center',
+        marginTop: 'bold'
+    },
+
+    buttonText:{
+        color: '#FFF',
+        fontSize: 17,
+        fontWeight: 'bold'
+    }
+
+}); //Fim dos Estilos
